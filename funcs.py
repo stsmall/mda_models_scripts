e0ba2713d745f63502546bc24a58576fa1dbb327
smalls <stsmall@gmail.com>
2017-01-09 17:56:27 -0500

    test complete for 2 and 3 villages
    note: check code at 185-191 couldnt think of a better way
    note: parsing func 199-211 is very slow when long locus, to do parse_ms_output()

--- funcs.py
+++ funcs.py
@@ -1,4 +1,3 @@
-import time
 import math
 import subprocess
 from collections import defaultdict
@@ -9,7 +8,7 @@ import numpy as np
 def migration_matrix(villages, initial_migration, initial_distance_m, theta,
                      basepairs, mutation):
     '''Creates a string that represents a migration matrix between
-    metapopulations.
+    metapopulations. Migration here is a stepping stone not island model
 
     Format specified by ms (hudson 2000). Uses euclidian distances. The value
     of 2Nm is weighted by the distance from the next population as an
@@ -30,8 +29,6 @@ def migration_matrix(villages, initial_migration, initial_distance_m, theta,
     Migration matrix: string
         migration matrix for scrm
     '''
-
-    print("starting migration_matrix")
     ne = theta / (4 * mutation * basepairs)
     # cant figure out how to pythonically increase the villages without just
     # using loops
@@ -80,11 +77,8 @@ def ms_outcall(
         mutation,
         recombination,
         time2Ancestral,
-        thetaAncestral,
         thetaRegional,
-        time_join12,
-        time_join23,
-        time_join34):
+        time_join):
     '''External call to ms (Hudson 2000) or scrm.
 
     Calls the function migration_matrix() if using more than 1 village.
@@ -109,24 +103,16 @@ def ms_outcall(
     recombination:(list,float)
         recombination rate for each locus as probability per base 
         per generation
-    thetaAncestral:float
-        theta for ancestral pops to the ratio of N0; e.g.,
     thetaRegional: float
         theta for regional pops to the ratio of N0 e.g.,23 times larger
     time2Ancestral: int
         time in generations to the ancestral population
-    time_join12: int
-        time in generations for joining/splitting pop 1 into 2
-    time_join23: int
-        time in generations for joining/splitting pop 2 into 3
-    time_join34: int
-        time in generations for joining/splitting pop 3 into 4
+    time_join: int
+        time in generations for joining/splitting villages
 
     Returns
     -------
     '''
-    print("starting scrm...")
-    t0 = time.clock()
     num_loci = len(theta)
     # theta for first population, all other thetas are scaled from this value
     thetaN0 = [t[0] for t in theta]
@@ -136,18 +122,11 @@ def ms_outcall(
     # time to the ancestral population
     tA = [b * (time2Ancestral / (t / m))
           for b, t, m in zip(basepairs, thetaN0, mutation)]
-    # time to join 1 & 2
-    t12 = [b * (time_join12 / (t / m))
-           for b, t, m in zip(basepairs, thetaN0, mutation)]
-    # time to join 2 & 3
-    t23 = [b * (time_join23 / (t / m))
-           for b, t, m in zip(basepairs, thetaN0, mutation)]
-    # time to join 3 & 4
-    t34 = [b * (time_join34 / (t / m))
+    # time to join villages
+    tjoin = [b * (time_join / (t / m))
            for b, t, m in zip(basepairs, thetaN0, mutation)]
     # list intialization for recording haplotypes
     hap_pop = defaultdict(list)
-    time_join = [t12, t23, t34]
 
     for loc in range(0, num_loci):
         if rho[loc] is 0:
@@ -166,42 +145,57 @@ def ms_outcall(
             'time_growth': tA[loc],
             'sig_digits': 12,
         }
-        scrm_base = ("scrm {nhaps} 1 -t {theta} -r {rho} {basepair} "
+
+        #order matters for the command call
+        if villages == 1:
+            scrm_base = ("scrm {nhaps} 1 -t {theta} -r {rho} {basepair} "
                      "-G {exp_growth} -eG {time_growth} 0.0 -SC abs -p"
                      " {sig_digits} ")
-
-        if villages == 1:
             mscmd = scrm_base.format(**ms_params)
         else:  # ms setup for >1 villages
             num_subpops = len(worm_popsize)  # -I num_pops
             sub_pop = " ".join(map(str, worm_popsize))  # -I X i j ...
-            mscmd = scrm_base.format(**ms_params)
-            for village_ix in range(villages):
-                mm = migration_matrix(
-                    villages,
-                    initial_migration,
-                    initial_distance_m,
-                    thetaN0[loc],
-                    basepairs[loc],
-                    mutation[loc])
-                if village_ix == 0:
-                    present_pop = 1
-                else:
-                    present_pop = float(thetaN0[loc]) / theta[loc][village_ix]
-                    mscmd += '-ej {0} {1} {2}'.format(
-                        time_join[
-                            village_ix - 1][loc],
-                        village_ix + 1,
-                        village_ix + 2)
-                mscmd += '-n {} {} '.format(village_ix, present_pop)
+            mm = migration_matrix(
+               villages,
+               initial_migration,
+               initial_distance_m,
+               thetaN0[loc],
+               basepairs[loc],
+               mutation[loc])
+            
             if villages == 2:
-                # Island model
-                mscmd += '-I {} {} {} '.format(num_subpops, sub_pop, mm)
-            else:
-                mscmd += 'I {} {} '.format(num_subpops, sub_pop)
-                mscmd += '-ma {} '.format(mm)
+                 scrm_base = ("scrm {nhaps} 1 -t {theta} -r {rho} {basepair} "
+                     "{sub_pop} {present_pop} {join} -G {exp_growth} -eG {time_growth} 0.0 -SC abs -p"
+                     " {sig_digits} ")
+                 ms_params['present_pop'] = '-n 1 1 -n 2 {}'.format(float(theta[loc][1])/thetaN0[loc])
+                 ms_params['sub_pop'] = '-I {} {} {}'.format(num_subpops, sub_pop, mm)
+                 ms_params['join'] = '-ej {} 1 2'.format(tjoin[loc])
+                 mscmd = scrm_base.format(**ms_params)
+            else:     
+                 scrm_base = ("scrm {nhaps} 1 -t {theta} -r {rho} {basepair} "
+                     "{sub_pop} {present_pop} {ma} {join} -G {exp_growth} -eG {time_growth} 0.0 -SC abs -p"
+                     " {sig_digits} ")
+                 ms_params['sub_pop'] = '-I {} {}'.format(num_subpops, sub_pop)
+                 ms_params['ma'] = '-ma {} '.format(mm)
+                 joinstr = ''
+                 subpopstr = ''
+                 for village_ix in range(villages):
+                    present_pop = float(theta[loc][village_ix])/thetaN0[loc]
+                    subpopstr += '-n {} {} '.format(village_ix + 1, present_pop)
+###dont want last set of -ej time 3 4 # for villages of 3 since there is no 4 to join
+                    if village_ix != villages -1:
+                         joinstr += '-ej {0} {1} {2} '.format(
+                             tjoin[loc],
+                             village_ix + 1,
+                             village_ix + 2)
+###this was a sloppy way of avoiding it, any ideas?
+                 ms_params['present_pop'] = subpopstr
+                 ms_params['join'] = joinstr
+                 mscmd = scrm_base.format(**ms_params)     
+        
         print(mscmd)
         proc = subprocess.Popen(mscmd, shell=True, stdout=subprocess.PIPE)
+
         # parses ms output from stdout
         for line in iter(proc.stdout.readline, ''):
             if line.startswith("positions"):
@@ -215,9 +209,21 @@ def ms_outcall(
                     ti = [positions[j] for j in [m.start() 
                         for m in re.finditer("1", hap)]]
                     hap_pop["locus" + str(loc)].append(ti)
-    print(time.clock() - t0)
     return hap_pop
     #{'locus0':[14.0, 26.0],[5.0, 7.0]]}
 
 if __name__ == '__main__':
-    ms_outcall()
+#2villages
+     ms_outcall([100,100], 2, 0.0001, [1000], [[5, 5], [10, 10]], [13000, 200000], 
+                [7.6E-8, 2.9E-9],  [0, 2.9E-9], 1800, 23, 240)
+#3villages
+     #ms_outcall([100,100,100], 3, 0.0001, [1000,2000,500], [[5, 2, 2], 
+      #           [10, 12, 12]], [13000, 200000], [7.6E-8, 2.9E-9],  
+       #          [0, 2.9E-9], 1800, 23, 240)
+    
+    
+    
+    
+    
+    
+    
