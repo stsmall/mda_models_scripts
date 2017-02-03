0040bd7e5e3dc401241b59e2807118bf6485ab0c
smalls <stsmall@gmail.com>
2017-02-03 12:17:35 -0500

    added statement to prevent gene coords outside of locus

--- filtercoords.py
+++ filtercoords.py
@@ -34,29 +34,24 @@ def filtercoords_fx(positions, basepairs, perc_locus, cds_length, intgen_length)
     #last coordinate is approx num_cds * mu; so if num_cds is too large or mu is too long
     #genes will run over the locus length
     for loc in range(len(positions)):
-        #print loc
         num_cds = int(round((perc_locus[loc + 1]*basepairs[loc + 1]) / cds_length))
-        #print num_cds
         size_cds = np.round(np.random.gamma(4, 0.25, num_cds) * cds_length)
-        #print size_cds
         #r = size
         #m = mean
         #p = r / (  r + m )
         cds_between = np.random.negative_binomial(size, size/float(mu+size), num_cds)
-        #print cds_between
         cds_stop = 0
-        #print cds_stop
         cds_coords = []
         for i, j in zip(cds_between, size_cds):
             #[i + cds_stop, i + cds_stop + j]
-            cds_coords.append([i + cds_stop, i + j + cds_stop])
-            cds_stop += (i + j)
-            #print i, j, cds_stop
+            if (i + cds_stop > basepairs[loc + 1]) or (i + j + cds_stop > basepairs[loc + 1]):
+                break
+            else:
+                cds_coords.append([i + cds_stop, i + j + cds_stop])
+                cds_stop += (i + j)
             
-        #np.where(cds_coords > basepairs[loc])
         keep_pos = []
         for start, end in cds_coords:
-            #need to properly index positions for each locus; positions[loc]
             keep_pos.extend(positions[loc][np.where(np.logical_and(positions[loc] >= start, positions[loc] <= end))]) 
         cds_positions.append(keep_pos) #this is a nested list for each locus
         cds_coordinates.append(cds_coords) #this is a nested list for each locus
